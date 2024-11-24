a6fa1da391c319b148ec2e4dc3d7615714b8ef9b
garfield <admin@g4rf.net>
2024-11-24 13:45:43 +0100

    changed recipient field to textarea

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -2,7 +2,7 @@
  * © 2018 Jan Kossick, jankossick@online.de, https://jankossick.de
  */
 
-/* global localforage, parseFloat */
+/* global localforage, parseFloat, Config */
 
 /** helper **/
 var FormsHelper = {
@@ -17,22 +17,23 @@ $("#privacy-policy").attr("href", Config.privacyPolicy);
 jQuery.each(Config.autoFill, function(key, value) {
     var elem = $(".auto-fill." + key);
     $(".auto-fill." + key).each(function() {
-        if($(this).is("input")) {
+        if($(this).is("input, textarea")) {
             $(this).val(value);
         } else {
-            $(this).empty().append(value);
+            $(this).empty().append(value.replaceAll("\n", "<br />"));
         }
     });
 });
 
 /** inputs magic: save values to printable spans **/
-$("body").on("keyup", "label input", function() {
-    var label = $(this).parent();
-    $("span.only-print", label).empty().append($(this).val());
+$("body").on("keyup", "label input, label textarea", function() {
+    const label = $(this).parent();
+    const value = $(this).val().replaceAll("\n", "<br />");
+    $("span.only-print", label).empty().append(value);
 });    
 
 /** inputs magic: save values to localStorage **/
-$("body").on("change", "input.save-data", function() {
+$("body").on("change", ".save-data", function() {
     if($("#save-data input").prop("checked")) {
         storage.setItem($(this).data("key"), $(this).val());
     }
@@ -45,7 +46,7 @@ $(".print-button").click(function() {
 $("#save-data input").change(function() {
     if(this.checked) { // save values
         storage.setItem("save-data", true);
-        $("input.save-data").each(function() {
+        $(".save-data").each(function() {
             console.log(this);
             storage.setItem($(this).data("key"), $(this).val());
         });
